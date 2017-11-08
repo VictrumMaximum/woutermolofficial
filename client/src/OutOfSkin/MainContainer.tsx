57605645d01532c377bc9268695bfa5eb01212f6
Victor Ionescu <v.ionescu@student.tudelft.nl>
2017-11-08 19:21:52 +0100

    remove musiccontainer

--- client/src/OutOfSkin/MainContainer.tsx
+++ client/src/OutOfSkin/MainContainer.tsx
@@ -1,7 +1,5 @@
 import * as React from "react";
 import Menu from "../Static/MenuBar/MenuBar";
-import TourContainer from "../TourMenu/MainContainer";
-import MusicContainer from "../MusicContainer";
 require("../../media/images/tour.jpg");
 
 export default class MainContainer extends React.Component<{}, {}> {
@@ -18,12 +16,6 @@ export default class MainContainer extends React.Component<{}, {}> {
 				<img src="../../media/images/tour.jpg" />
 				<div className="row justify-content-center" style={{height: "100vh", backgroundImage: fullBackgroundPath}}>
 					<Menu />
-					{/*<div>*/}
-						{/*<TourContainer />*/}
-					{/*</div>*/}
-					<div>
-						<MusicContainer />
-					</div>
 				</div>
 			</div>
 		);
